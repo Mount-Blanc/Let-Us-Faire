f901aec3d415a8c5998820b92f88275d0fd5c522
Mount-Blanc <mountblanc87@gmail.com>
2023-01-29 10:07:16 -0800

    added add to do feature

--- letusfaire/src/App.js
+++ letusfaire/src/App.js
@@ -3,25 +3,39 @@ import {db} from './FirebaseConfig'
 import {collection, addDoc,getDocs } from "firebase/firestore"; 
 import {useState, useEffect} from 'react'
 function App() {
+
 const [todo, settodo] = useState([])
+const [addTodo, setaddTodo] = useState('')
+
+    const docRef =collection(db, "todos");
 
 useEffect( () => {
 
   const getTodos =  async () => {
-    const docRef =collection(db, "todos");
+
   const docSnap = await getDocs(docRef);
- settodo(docSnap.docs.map((doc) => ({...doc.data()}) )) 
+
+ settodo(docSnap.docs.map((doc) => ({...doc.data(),id:doc.id}) )) 
   };
 
   getTodos();
 }, []);
   
-
+const todoHandler = (event) => {
+  setaddTodo(event.target.value)
+}
+const addtoDocument= async () => {
+  settodo(addTodo)
+  await addDoc(docRef, {todo:addTodo})
+}
   return (
     <div className="App">
       hello
+      <input placeholder='Enter Todo' type="text" onChange={todoHandler}/>
+    <button onClick={addtoDocument}>Add</button>
       {todo.map((todos) => (
-         <div>{todos.todo}</div>
+         <div>{todos.todo}
+         {todos.completed}</div>
       ))}
     </div>
   );
