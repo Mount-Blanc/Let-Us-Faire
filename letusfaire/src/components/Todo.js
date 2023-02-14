d1e39d17dc82ce375083c4480f675ec1980023bd
Mount-Blanc <mountblanc87@gmail.com>
2023-02-14 15:35:04 -0600

    fixed firebase import

--- letusfaire/src/components/Todo.js
+++ letusfaire/src/components/Todo.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 import {collection, addDoc,getDocs,updateDoc,doc,deleteDoc } from "firebase/firestore"; 
-import {db} from '../FirebaseConfig'
+import {db} from '../../FirebaseConfig'
       import Button from 'react-bootstrap/Button';
 
       import './Todo.css'
